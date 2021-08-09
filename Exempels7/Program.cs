75e16aa73d76c04a9e61b27e372d19fe6bf4525e
Vinni949 <Pumba0@yandex.ru>
2021-08-09 17:10:06 +0300

    initialization

---
+++ Exempels7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace work7
+
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string writePath = @"C:\Users\pumba\source\repos\Exempels7\Class.txt";
+
+            int number = 1;
+            string Name = Console.ReadLine(); 
+            string FirstName = Console.ReadLine();
+            string Task = Console.ReadLine();
+            string data = DateTime.Now.ToString();
+            TaskBook book = new TaskBook(number,Name,FirstName,Task,data);
+            Console.WriteLine(DateTime.Now);
+            book.Print();
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    await sw.WriteLineAsync("");
+                    await sw.WriteAsync(book.GetNumber.ToString());
+                    await sw.WriteAsync("| " + book.GetName);
+                    await sw.WriteAsync("| " + book.GetFirstName);
+                    await sw.WriteAsync("| " + book.GetTask);
+                    await sw.WriteAsync("| " + book.GetDate);
+                    await sw.WriteAsync("|");
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exempels7/TaskBook.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work7
+{
+    class TaskBook
+    {
+        private int Number;
+        private string Name;
+        private string FirstName;
+        private string Task;
+        private string Data;
+
+        public TaskBook(int Number, string Name, string FirstName, string Task, string data)
+        {
+            this.Number = Number;
+            this.Name = Name;
+            this.FirstName = FirstName;
+            this.Task = Task;
+            this.Data = data;
+        }
+
+        public void EditingName(string NewName)
+        {
+            Name = NewName;
+        }
+        public void EditingNewTask(string NewTask)
+        {
+            Task = NewTask;
+        }
+        public void AddTask(string AddTask)
+        {
+            Task += AddTask;
+        }
+
+        public void AddTime(DateTime data)
+        {
+            Data += ", ";
+            Data += data;
+        }
+        public void Print()
+        {
+            Console.WriteLine(Number + "\n" + Name+"\n"+FirstName + "\n" + Task + "\n" + Data);
+        }
+        public string GetName
+        {
+            get
+            {
+                return Name;
+            }
+        }
+        public int GetNumber
+        {
+            get
+            {
+                return Number;
+            }
+        }
+        public string GetFirstName
+        {
+            get
+            {
+                return FirstName;
+            }
+        }
+        public string GetTask
+        {
+            get
+            {
+                return Task;
+            }
+        }
+        public string GetDate
+        {
+            get
+            {
+                return Data;
+            }
+        }
+    }
+}
